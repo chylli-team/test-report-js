bc1a9f665a401dd40aa3bc7a61fa5468fbfd3fd7
chylli-deriv <chylli@deriv.com>
2024-01-06 16:30:38 +0800

    remove source file

--- src/add.ts
+++
@@ -1,8 +0,0 @@
-export function add(a: number, b: number): number {
-    return a + b;
-}
-
-export function willThrowError(): number {
-    throw new Error('error');
-    return 1;
-}
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-// main.ts
-
-import { add } from './add';
-
-const result = add(5, 3);
-console.log(result);
