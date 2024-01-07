fb8c44e68e32269848c137793d62c6c9455e5cfd
chylli-deriv <chylli@deriv.com>
2024-01-07 17:19:53 +0800

    change describe

--- test/add.test.ts
+++ test/add.test.ts
@@ -21,7 +21,7 @@ describe('fle 1 describe 2',() => {
         expect(add(0, 0)).toBe(0);
         expect(add(-3, 3)).toBe(0);
     });
-    it('describe 2 it 2', () => {
+    it('describe 2 it 2 will fail', () => {
         expect(add(5, 3)).toBe(8);
         expect(add(0, 0)).toBe(0);
         expect(add(-3, 3)).toBe(1);
